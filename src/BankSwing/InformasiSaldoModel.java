43eba10f9fc7088794121ef21e83d8856f6cffe4
elvisusanti20 <114349571+elvisusanti20@users.noreply.github.com>
2023-05-30 17:54:49 +0700

    UTS pbo2 Elvi Susanti
    mengupload package bankswing Uts PBO2

---
+++ src/BankSwing/InformasiSaldoModel.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankSwing;
+
+/**
+ *
+ * @author M3400QA
+ */
+public class InformasiSaldoModel {
+    String saldo="986000000";
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/BankSwing/TransferModel.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankSwing;
+
+/**
+ *
+ * @author M3400QA
+ */
+public class TransferModel {
+    
+    String norek;
+    String jml;
+    int transfer;
+
+    public int getTransfer() {
+        return transfer;
+    }
+
+    public void setTransfer(int transfer) {
+        this.transfer = transfer;
+    }
+    
+    public String getNorek() {
+        return norek;
+    }
+
+    public void setNorek(String norek) {
+        this.norek = norek;
+    }
+
+    public String getJml() {
+        return jml;
+    }
+
+    public void setJml(String jml) {
+        this.jml = jml;
+    }
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setText(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+}
+}
